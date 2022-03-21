36e821fb9c86a899d3a5a9eee5166fa999fb7ee5
Sean <jetrospect@outlook.com>
2022-03-20 20:49:47 -0400

    added-particle-text

--- 03-19-2022Overlay/overlay.js
+++ 03-19-2022Overlay/overlay.js
@@ -3,19 +3,19 @@ const 	subMultiplier = 1,
 		rotation = 90,
 		gap = 56,
 		range = 360-gap,
-		increment = range/100,
-		subWeight = 10*subMultiplier, 
-		bitWeight = 0.02*bitMultiplier,
+		increment = range / 100,
+		subWeight = 10 * subMultiplier, 
+		bitWeight = 0.02 * bitMultiplier,
 		progressBounds = {
-			start: gap/2+rotation,
-			end: gap/2+rotation+range
+			start: gap / 2 + rotation,
+			end: gap / 2+ rotation + range
 		};
 
 
-let 	subQuantity = 9,
-		bitQuantity = 500,
-		supportMultiplier = [(subWeight*subQuantity)+(bitWeight*bitQuantity)]*increment,
-		currentProgress = progressBounds.start+supportMultiplier,
+let 	subQuantity = 3,
+		bitQuantity = 364,
+		supportMultiplier = [(subWeight * subQuantity) + (bitWeight * bitQuantity)] * increment,
+		currentProgress = progressBounds.start + supportMultiplier,
 		percentage = supportMultiplier / increment;
 		roundedPercentage = percentage.toFixed(2);
 		barThickness = 2 + (supportMultiplier * 2 / range),
@@ -49,7 +49,7 @@ function setup() {
 
 //Progress bar element		
 function draw() {
-			stroke(128, 128, 128, 50);
+			stroke(180, 180, 180, 80);
 			strokeWeight(trackThickness);
 			noFill();
 			arc(barWidth/2, barHeight/2, 96, 96, progressBounds.start, progressBounds.end);
@@ -70,96 +70,109 @@ const particleCanvas = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particleArray = [];
+let adjustX = 30;
+let adjustY = 240;
 
 //Blade path for cutting through supporter text.
-let blade = {
+const blade = {
 	x: null,
 	y: null,
+	radius: 20
 }
 
 window.addEventListener('mousemove', function(position){
-	blade.x = position.x
-	blade.y = position.y
+	blade.x = position.x;
+	blade.y = position.y;
 console.log(blade.x, blade.y)
 })
 
-particleCanvas.fillStyle = 'Green';
-particleCanvas.font = '30px Tahoma';
-particleCanvas.fillText('Alert Username', 222, 680, 1920);
-const textData = particleCanvas.getImageData(0, 0, 100, 100)
+particleCanvas.fillStyle = 'green';
+particleCanvas.font = '32px Tahoma';
+particleCanvas.fillText('Alert Username', 30, 72, 2560);
+// particleCanvas.strokeStyle = 'white';
+// particleCanvas.strokeRect(0, 0, 400, 400)
+const textCoordinates = particleCanvas.getImageData(0, 0, 2160, 2160);
 
+class Particle {
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+		this.size = 3;
+		//saving position for particles to return to
+		this.baseX = this.x;
+		this.baseY = this.y;
+		this.denisty = (Math.random() * 80) + 1;
+		
+	}
+	draw(){
+		particleCanvas.fillStyle = 'cyan';
+		particleCanvas.beginPath();
+		particleCanvas.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+		particleCanvas.closePath();
+		particleCanvas.fill();
+	}
+	update(){
+		let dx = blade.x - this.x;
+		let dy = blade.y - this.y;
+		let distance = Math.sqrt(dx * dx + dy * dy)
+		let forceDirectionX = dx / distance;
+		let forceDirectionY = dy / distance;
+		let maxDistance = blade.radius;
+		let force = (maxDistance - distance) / maxDistance;
+		let directionX = forceDirectionX * force * this.denisty;
+		let directionY = forceDirectionY * force * this.denisty;
+		if (distance < blade.radius){
+			this.size = 1;
+			this.x -= directionX;
+			this.y -= directionY;
+		} else {
+			if (this.x !== this.baseX){
+				let dx = this.x - this.baseX;
+				this.x -= dx/160;
+				this.size = 1;
+			}
+			if (this.y !== this.baseY){
+				let dy = this.y - this.baseY;
+				this.y -= dy/160;
+				this.size = 1;
+			}
+			this.size = 1.4;
+		}
+	}
+}
 
+function initialize() {
+	particleArray = [];
+	for (let y = 0, y2 = textCoordinates.height; y < y2; y++){
+		for (let x = 0, x2 = textCoordinates.width; x < x2; x++){
+			if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x + 4) + 3] > 208){
+				let positionX = x + adjustX;
+				let positionY = y + adjustY;
+				particleArray.push(new Particle(positionX * 0.6, positionY * 2.4));
+			}
+		}
+	}
 
-// window.addEventListener('mousemove', function(position){
-// 	blade.x = position.x;
-// 	blade.y = position.y;
-// 	blade.radius = 150;
-// 	// console.log(blade.x, blade.y)
-// });
+	// for (let i = 0; i < 400; i++){
+	// 	let x = Math.random() * canvas.width/2;
+	// 	let y = Math.random() * canvas.height/2;
+	// 	particleArray.push(new Particle(x, y));
+	// }
+}
 
-// particleCanvas.fillStyle = 'Green';
-// particleCanvas.font = '30px Tahoma';
-// particleCanvas.fillText('Alert Username', 0, 30);
-// const data = particleCanvas.getImageData(0, 0, 100, 100);
-
-// class Particle {
-// 	constructor(x, y){
-// 		this.x = x;
-// 		this.y = y;
-// 		this.size = 3;
-// 		this.baseX = this.x;
-// 		this.baseY = this.y;
-// 		this.density = (Math.random() * 30) + 1;
-// 	}
-// 	draw(){
-// 		particleCanvas.fillStyle = 'cyan';
-// 		particleCanvas.beginPath();
-// 		particleCanvas.arc(this.x, this.y, this.size, 0, Math.PI *2);
-// 		particleCanvas.closePath();
-// 		particleCanvas.fill();
-// 	}
-// 	update(){
-// 		let dx = blade.x - this.x;
-// 		let dy = blade.y - this.y;
-// 		let distance = Math.sqrt(dx * dx + dy * dy);
-// 		let forceDirectionX = dx / distance;
-// 		let forceDirectionY = dy / distance;
-// 		let maxDistance = blade.radius;
-// 		let force = (maxDistance - distance) / maxDistance;
-// 		let directionX = forceDirectionX * force * this.density;
-// 		let directionY = forceDirectionY * force * this.density;
-
-// 		if (distance < blade.radius){
-// 			this.x -= directionX;
-// 			this.y -= directionY;
-// 		} else {
-// 			this.size = 3;
-// 		}
-// 	}
-// }
-
-// function initialize() {
-// 	particleArray = [];
-// 	for (let i = 0; i < 1000; i++){
-// 		let x = Math.random() * canvas.width;
-// 		let y = Math.random() * canvas.height;
-// 		particleArray.push(new Particle(x, y));
-// 	}
-
-// }
-
-// initialize();
+initialize();
 // console.log(particleArray);
 
-// function animate(){
-// 	particleCanvas.clearRect(0, 0, canvas.width, canvas.height);
-// 	for (let i = 0; i < particleArray.length; i++){
-// 		particleArray[i].draw();
-// 		particleArray[i].update();
-// 	}
-// 	requestAnimationFrame(animate);
-// }
-// animate();
+function animate(){
+	particleCanvas.clearRect(0, 0, canvas.width, canvas.height);
+	for (let i = 0; i < particleArray.length; i++){
+		particleArray[i].draw();
+		particleArray[i].update();
+	}
+	requestAnimationFrame(animate);
+}
+
+animate();
 
 
 
