f36bb362ab6a98c7f7e7644c5d869016ce963190
Sean <jetrospect@outlook.com>
2022-04-06 22:26:23 -0400

    incremented-percentage

--- 03-19-2022Overlay/progressBar.js
+++ 03-19-2022Overlay/progressBar.js
@@ -1,69 +1,84 @@
-const 	followMultiplier = 1,
-		subMultiplier = 1,
-		bitMultiplier = 1,
-		rotation = 90,
-		gap = 56,
-		range = 360-gap,
-		increment = range / 100,
-		followWeight = 1 * followMultiplier,
-		subWeight = 10 * subMultiplier, 
-		bitWeight = 0.02 * bitMultiplier,
-		progressBounds = {
-			start: gap / 2 + rotation,
-			end: gap / 2+ rotation + range
-		};
+const bar = new Object();
 
-let 	subQuantity = 1,
-		bitQuantity = 500,
-		supportMultiplier = [(subWeight * subQuantity) + (bitWeight * bitQuantity)] * increment,
-		currentProgress = progressBounds.start + supportMultiplier,
-		percentage = supportMultiplier / increment;
-		roundedPercentage = percentage.toFixed(2);
-		barThickness = 2 + (supportMultiplier * 2 / range),
-		trackThickness = 1 + (supportMultiplier * 2 / range);
-		barRed = 0 + (supportMultiplier * 80 / range);
-		barGreen = 120 + (supportMultiplier * 175 / range);
-		barBlue = 120 + (supportMultiplier * 175 / range);
-		barRGB = `${barRed}, ${barGreen}, ${barBlue}`;
+bar.rotation = 90;
+bar.gap = 56;
+bar.width = 96;
+bar.height = 96;
+bar.followMultiplier = 1;
+bar.subMultiplier = 1;
+bar.cheerMultiplier = 1;
+bar.follows = 4;
+bar.subs = 0;
+bar.cheers = 380;
+bar.range = 360-bar.gap;
+bar.increment = bar.range / 100;
+bar.start = bar.gap / 2 + bar.rotation;
+bar.end = bar.gap / 2+ bar.rotation + bar.range;
+bar.followWeight = 4*bar.followMultiplier;
+bar.subWeight = 10*bar.subMultiplier;
+bar.cheerWeight = 0.02*bar.cheerMultiplier;
+bar.multiplier = [(bar.follows * bar.followWeight) + (bar.subs * bar.subWeight) + (bar.cheers * bar.cheerWeight)] * bar.increment;
+bar.current = bar.start + bar.multiplier;
+bar.percentage = Math.floor((bar.multiplier / bar.increment)*100);
+bar.thickness = 2 + (bar.multiplier * 2 / bar.range);
+bar.frame = 1 + (bar.multiplier * 2 / bar.range);
+bar.red = 0 + (bar.multiplier * 80 / bar.range);
+bar.green = 120 + (bar.multiplier * 175 / bar.range);
+bar.blue = 120 + (bar.multiplier * 175 / bar.range);
+bar.color = `${bar.red}, ${bar.green}, ${bar.blue}`;
 
-console.log(`Bar Increment: ${increment},
-Percentage Completed: ${percentage},
-Support Multiplier: ${supportMultiplier},
-Bar Range: ${range},
-Bar RGB: ${barRGB}`);
+console.log(bar)
 
-//Progress bar canvas
-document.querySelector('#defaultAnimation').getBoundingClientRect().left // X
-document.querySelector('#defaultAnimation').getBoundingClientRect().top // X
-let barRect = defaultAnimation.getBoundingClientRect();
-let barWidth = 128;
-let barHeight = 128;
+const ring = function(sketch){
+	let x = window.innerWidth;
+	let y = window.innerHeight;
 
-function setup() {
-    angleMode(DEGREES);
-	let canvas = createCanvas(barWidth, barHeight);
-	let x = (windowWidth - width) / 2;
-	let y = (windowHeight - height) / 2;
-	canvas.position(x, y);
+	//Progress canvas
+	sketch.setup = function(){
+		sketch.angleMode(sketch.DEGREES);
+		sketch.createCanvas(x, y);
+	}
+
+	//Progress element	
+	sketch.draw = function(){
+		sketch.stroke(192, 192, 192, 192);
+		sketch.strokeWeight(bar.frame);
+		sketch.noFill();
+		sketch.arc(x/2, y/2, bar.width, bar.height, bar.start, bar.end);
+		sketch.stroke(bar.red, bar.green, bar.blue);
+		sketch.strokeWeight(bar.thickness);
+		sketch.noFill();
+		sketch.arc(x/2, y/2, bar.width, bar.height, bar.start, bar.current);
+		sketch.noLoop();
+	}
 }
 
-//Progress bar element		
-       
-function draw() {
-	stroke(180, 180, 180, 80);
-	strokeWeight(trackThickness);
-	noFill();
-	arc(barWidth/2, barHeight/2, 96, 96, progressBounds.start, progressBounds.end);
-	stroke(barRed, barGreen, barBlue);
-	strokeWeight(barThickness);
-	noFill();
-	arc(barWidth/2, barHeight/2, 96, 96, progressBounds.start, currentProgress);
-	noLoop();
-    redraw();
+let progress = new p5(ring);
+
+let incrementRates = [1, 20, 50, 80, 100]
+
+
+bar.maximum = 10000
+bar.change = 7000
+
+bar.display = (bar.percentage/100).toFixed(2)
+bar.update = $('#percentage').text(bar.display)
+
+function increment(){
+	let value = +bar.update.text()*100;
+	console.log(value);
+	if (value < bar.maximum){
+		value += bar.change/100;
+		display = (value/100).toFixed(2);
+		bar.update.text(display);
+	} else {
+		bar.update.text((bar.maximum/100).toFixed(2))
+	}
+	setTimeout(increment, bar.maximum/1000)
 }
 
-const progressPercentage = document.getElementById('progressPercentage');
-progressPercentage.innerHTML = `${roundedPercentage}`;
+
+increment();
 
 
 
